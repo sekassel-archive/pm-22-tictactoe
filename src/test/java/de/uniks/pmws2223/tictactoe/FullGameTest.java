897c5b14030bc00080a366b855816a3a09f91a9c
Giulcoo <74862175+Giulcoo@users.noreply.github.com>
2023-01-14 13:01:30 +0100

    Finished 2.4

--- src/test/java/de/uniks/pmws2223/tictactoe/FullGameTest.java
+++ src/test/java/de/uniks/pmws2223/tictactoe/FullGameTest.java
@@ -1,8 +1,11 @@
 package de.uniks.pmws2223.tictactoe;
 
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
+import static org.junit.Assert.*;
+import static de.uniks.pmws2223.tictactoe.Constants.*;
 
 public class FullGameTest extends ApplicationTest {
     private App app;
@@ -17,6 +20,52 @@ public class FullGameTest extends ApplicationTest {
 
     @Test
     public void fullGame() {
-        // TODO
+        // Setup Screen
+        assertEquals(SETUP_TITLE, stage.getTitle());
+
+        // Check that game can not be started with empty names
+        clickOn("#startButton");
+        assertEquals(SETUP_TITLE, stage.getTitle());
+
+        clickOn("#playerNameX");
+        write("Alice");
+        clickOn("#startButton");
+        assertEquals(SETUP_TITLE, stage.getTitle());
+
+        clickOn("#playerNameO");
+        write("Bob");
+        clickOn("#startButton");
+
+        // Ingame Screen
+        assertEquals(INGAME_TITLE, stage.getTitle());
+
+        // Check that Alice starts the game
+        Label currentPlayerLabel = lookup("#currentPlayerLabel").query();
+        assertEquals(currentPlayerLabel.getText(), CURRENT_PLAYER_TEXT + "Alice");
+
+        // Let Alice win the game
+        clickOn("#r1c1");
+
+        // Check that nothing happens when player clicks on occupied field
+        assertEquals(currentPlayerLabel.getText(), CURRENT_PLAYER_TEXT + "Bob");
+        clickOn("#r1c1");
+        assertEquals(currentPlayerLabel.getText(), CURRENT_PLAYER_TEXT + "Bob");
+        clickOn("#r2c1");
+
+        assertEquals(currentPlayerLabel.getText(), CURRENT_PLAYER_TEXT + "Alice");
+        clickOn("#r2c2");
+
+        assertEquals(currentPlayerLabel.getText(), CURRENT_PLAYER_TEXT + "Bob");
+        clickOn("#r3c1");
+
+        assertEquals(currentPlayerLabel.getText(), CURRENT_PLAYER_TEXT + "Alice");
+        clickOn("#r3c3");
+
+        assertEquals(currentPlayerLabel.getText(), WINNER_TEXT + "Alice");
+
+        clickOn("#leaveButton");
+
+        // Setup Screen
+        assertEquals(SETUP_TITLE, stage.getTitle());
     }
 }
--- src/test/java/de/uniks/pmws2223/tictactoe/service/GameServiceTest.java
+++ src/test/java/de/uniks/pmws2223/tictactoe/service/GameServiceTest.java
@@ -1,4 +1,134 @@
 package de.uniks.pmws2223.tictactoe.service;
 
+import de.uniks.pmws2223.tictactoe.model.Field;
+import de.uniks.pmws2223.tictactoe.model.Game;
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class GameServiceTest {
+    GameService gameService = new GameService("Alice", "Bob");
+    Game game = gameService.getGame();
+    List<Field> fields = game.getFields();
+
+    @Test
+    public void placeSymbol(){
+        // Check if currentPlayer switch works
+        assertEquals(game.getCurrentPlayer().getName(), "Alice");
+        gameService.placeSymbol(fields.get(0));
+        assertEquals(game.getCurrentPlayer().getName(), "Bob");
+
+        // Check if symbol can not be placed at same place by other player
+        // currentPlayer should stay the same
+        gameService.placeSymbol(fields.get(0));
+        assertEquals(game.getCurrentPlayer().getName(), "Bob");
+        assertEquals(fields.get(0).getPlayer().getName(), "Alice");
+
+        gameService.placeSymbol(fields.get(1));
+
+        // Check if symbol can not be placed at same place by same player
+        assertEquals(game.getCurrentPlayer().getName(), "Alice");
+        gameService.placeSymbol(fields.get(0));
+        assertEquals(game.getCurrentPlayer().getName(), "Alice");
+        assertEquals(fields.get(0).getPlayer().getName(), "Alice");
+
+        // Set Alice as the winner
+        gameService.placeSymbol(fields.get(3));
+        assertEquals(game.getCurrentPlayer().getName(), "Bob");
+        gameService.placeSymbol(fields.get(4));
+        assertEquals(game.getCurrentPlayer().getName(), "Alice");
+        gameService.placeSymbol(fields.get(6));
+        assertNotNull(game.getWinner());
+        assertNull(game.getCurrentPlayer());
+
+        // Check if nothing happens when the winner is set
+        gameService.placeSymbol(fields.get(8));
+        assertNotNull(game.getWinner());
+        assertNull(game.getCurrentPlayer());
+        assertNull(fields.get(8).getPlayer());
+    }
+
+    @Test
+    public void checkWinner(){
+        // In the beginning there should not be any winner
+        assertNull(game.getWinner());
+
+        // Let Alice win with a horizontal row
+        gameService.placeSymbol(fields.get(0));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(3));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(1));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(4));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(2));
+        assertEquals(game.getWinner().getName(), "Alice");
+
+        // Start new game and let Bob win vertically
+        gameService = new GameService("Alice", "Bob");
+        game = gameService.getGame();
+        fields = game.getFields();
+
+        gameService.placeSymbol(fields.get(0));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(2));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(1));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(5));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(3));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(8));
+        assertEquals(game.getWinner().getName(), "Bob");
+
+        // Start new game and let Alice win diagonally
+        gameService = new GameService("Alice", "Bob");
+        game = gameService.getGame();
+        fields = game.getFields();
+
+        gameService.placeSymbol(fields.get(0));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(3));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(4));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(6));
+        assertNull(game.getWinner());
+        gameService.placeSymbol(fields.get(8));
+        assertEquals(game.getWinner().getName(), "Alice");
+    }
+
+    @Test
+    public void checkDraw(){
+        // Call checkDraw by using placeSymbol
+        gameService.placeSymbol(fields.get(0));
+
+        // Check if checkDraw detects, that the field is not full
+        // If the field is full currentPlayer is set to null
+        assertNotNull(game.getCurrentPlayer());
+
+        // Place rest of symbols on the fields without letting anybody win
+        gameService.placeSymbol(fields.get(1));
+        assertNotNull(game.getCurrentPlayer());
+        gameService.placeSymbol(fields.get(3));
+        assertNotNull(game.getCurrentPlayer());
+        gameService.placeSymbol(fields.get(6));
+        assertNotNull(game.getCurrentPlayer());
+        gameService.placeSymbol(fields.get(2));
+        assertNotNull(game.getCurrentPlayer());
+        gameService.placeSymbol(fields.get(5));
+        assertNotNull(game.getCurrentPlayer());
+        gameService.placeSymbol(fields.get(7));
+        assertNotNull(game.getCurrentPlayer());
+        gameService.placeSymbol(fields.get(4));
+        assertNotNull(game.getCurrentPlayer());
+
+        // Now only one field is empty. If this field gets occupied, then the game should be
+        // over with a draw. currentPlayer should be null
+        gameService.placeSymbol(fields.get(8));
+        assertNull(game.getCurrentPlayer());
+    }
 }
